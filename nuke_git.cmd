398fcdfe850fb1725269796db4e0460236aca872
heniu75 <heinrich.van.vught@gmail.com>
2021-03-06 16:55:43 +0000

    Update nuke_git.cmd

--- nuke_git.cmd
+++ nuke_git.cmd
@@ -13,7 +13,7 @@ goto :eof
 
 :treeProcess
 rem for each .git folder found in the current folder, rimraf it
-for /D %%d in (*.git*) do rimraf %%d
+for /D %%d in (.git) do call :folderProcess %%d
 
 rem for each sub-folder, go into it and recurse
 for /D %%d in (*) do (
@@ -22,3 +22,12 @@ for /D %%d in (*) do (
     cd ..
 )
 exit /b
+
+:folderProcess
+@rem @echo processing %1 in %cd%
+@echo %cd%
+if exist %1 rimraf %1
+@rem if not exist %1 echo %1 does not exist
+exit /b
+
+exit /B 0
